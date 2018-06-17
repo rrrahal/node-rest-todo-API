0fbc3a52913d1133172a6519eb7a2ee6e5fbbeb0
Rafael Rahal <rrrahal@gmail.com>
2018-06-17 20:50:39 -0300

    add delete route and playground

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,19 @@
+const { mongoose } = require('./../server/db/mongoose');
+const { objectID } = require('mongodb');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+/* 
+Todo.remove({}).then((result) => {
+	console.log(result);
+}); */
+
+//Todo.findOneAndRemove()
+//Todo.findByIdAndRemove()
+
+Todo.findOneAndRemove({ _id: '5b26f15c1aa1944de96e479c' }).then((todo) => {
+	console.log(todo);
+});
+
+Todo.findByIdAndRemove('5b26f15c1aa1944de96e479c').then((todo) => {
+	console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -52,6 +52,27 @@ app.get('/todos/:id', (req, res) => {
 		});
 });
 
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id)
+		.then((todo) => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			return res.status(200).send({ todo });
+		})
+		.catch((e) => {
+			res.status(400).send();
+		});
+
+	//validate the id -> not valid 404
+
+	//remove todo by ID
+});
+
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
